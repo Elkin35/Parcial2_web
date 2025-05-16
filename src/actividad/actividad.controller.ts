5dd62022632f90a946eb7f6d368848575d34dccb
Elkin Cuello <e.cuello@uniandes.edu.co>
2025-05-16 09:21:50 -0500

    bono

--- src/actividad/actividad.controller.ts
+++ src/actividad/actividad.controller.ts
@@ -7,6 +7,12 @@ import { UpdateActividadDto } from './dto/update-actividad.dto';
 export class ActividadController {
   constructor(private readonly actividadService: ActividadService) {}
 
-  
+  @Post(':actividadId/inscripcion/:usuarioId')
+  inscribirseActividad(
+    @Param('actividadId') actividadId: number,
+    @Param('usuarioId') usuarioId: number
+  ) {
+    return this.actividadService.inscribirseActividad(actividadId, usuarioId);
+  }
   
 }
